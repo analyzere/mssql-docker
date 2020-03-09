d6091e5dc86044f30d26092662851d03262eaa84
Brad MacPhee <brad@analyzere.com>
2020-03-09 18:11:15 -0300

    fix sample to use environment

--- oss-drivers/pyodbc/sample.py
+++ oss-drivers/pyodbc/sample.py
@@ -1,9 +1,10 @@
 import pyodbc
-server = '$DB_HOST'
-username = '$DB_USERNAME'
-password = '$DB_PASSWORD'
+import os
+server = os.environ['DB_HOST']
+username = os.environ['DB_USERNAME']
+password = os.environ['DB_PASSWORD']
 
-cnxn = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER='+server+';PORT=1443;UID='+username+';PWD='+ password)
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';UID='+username+';PWD='+ password)
 cursor = cnxn.cursor()
 
 print ('Using the following SQL Server version:')
@@ -11,3 +12,4 @@ tsql = "SELECT @@version;"
 with cursor.execute(tsql):
     row = cursor.fetchone()
     print (str(row[0]))
+
